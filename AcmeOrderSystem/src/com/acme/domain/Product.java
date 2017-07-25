e569a889bdfff566dd58b31b46c742cc6d85db57
jdiakiw <jdiakiw12@gmail.com>
2017-07-24 23:07:57 -0400

    Completion of Lab 13 7/24/2017

---
+++ AcmeOrderSystem/src/com/acme/domain/Product.java
@@ -0,0 +1,11 @@
+package com.acme.domain;
+
+public interface Product {
+	
+	public abstract String getName();
+	
+	public abstract void setName(String n);
+	
+	public abstract String toString();
+
+}
